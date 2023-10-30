3da9e5f0be3327bee6010d01f9048b59043c078e
Flames4Yan <149405883+Flames4Yan@users.noreply.github.com>
2023-10-30 23:33:23 +0800

    Add files via upload
    公司根据当月利润发奖金的程序

---
+++ FileName.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main() {
+	long long int p, i, l;
+	printf("请输入当月利润：");
+	scanf_s("%lld", &p);
+	l = p / 100000;
+	switch (l)
+	{
+	case 10:
+		i = (p - 1000000) * 0.01;
+		break;
+	case 6:
+		i = (p - 600000) * 0.015;
+		break;
+	case 4:
+		i = (p - 400000) * 0.03;
+		break;
+	case 2:
+		i = (p - 200000) * 0.05;
+		break;
+	case 1:
+		i = (p - 100000) * 0.075;
+		break;
+	case 0:
+		i = p * 0.1;
+		break;
+	default:
+		printf("输入值异常/n");
+		break;
+	}
+	printf("应发奖金数为：%lld", i);
+	return 0;
+}
+
+
+
+
